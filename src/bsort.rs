3457c7b1fa22bb25e3df2d5f48564d7a272791aa
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-11-11 18:36:29 +0900

    03

--- src/bsort.rs
+++ src/bsort.rs
@@ -3,9 +3,10 @@
 /// use crate::toa::bsort::bsort;
 ///
 /// let mut vec: Vec<i32> = vec![-1, 4, 8, 1, 10, 8, -2, 9];
-/// let  sorted: Vec<i32> = vec![-2, -1, 1, 4, 8, 8, 9, 10];
+/// let mut ans = vec.clone();
 /// bsort(&mut vec);
-/// assert_eq!(vec, sorted);
+/// ans.sort();
+/// assert_eq!(vec, ans);
 ///```
 pub fn bsort<T: Ord>(v: &mut [T]) {
     for i in 0..v.len() {
